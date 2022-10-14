c8a924ff933fb28c52221d2798492a62bd76a474
hp <hp@DESKTOP-44ALA5Q>
2022-10-14 10:32:58 -0600

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp2/BAL/DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConexionBD.Properties;
+using System.Configuration;
+using WindowsFormsApp2.Properties;
+using System.Data.SqlClient;
+
+namespace ConexionBD.CD
+{
+    public class DataBase
+    {
+        public static string getStrConnection()
+        {
+            return Settings.Default.empleadosConnectionString;
+        }
+
+        public SqlConnection getConnection()
+        {
+             SqlConnection Con = new SqlConnection(getStrConnection());
+            return Con;
+        }
+    }
+}
+
+
+
